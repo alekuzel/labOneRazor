dcc6be0b3704f61a563c98916ddcf238ac153ea0
Aleksandra Kuzeleva <a.brods@gmail.com>
2024-02-06 13:48:03 +0100

    Date and side navigation

---
+++ labOne/labOne/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace labOne.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
